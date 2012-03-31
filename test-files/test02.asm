4db7086dae07596c1258ec0a9253945c78f57fdd
Samuel Schneider <crazysaem@googlemail.com>
2012-03-31 16:12:36 +0200

    ..

--- test-files/test02.asm
+++ test-files/test02.asm
@@ -24,15 +24,24 @@ _start:
 	SW $fp, 0($sp)
 	MOVE $fp, $sp
 
-	ADDI $sp, $sp, -8	#Allocate Memory on stackpointer for local Variables
-	#int i: 4($sp)
+	ADDI $sp, $sp, -48	#Allocate Memory on stackpointer for local Variables
+	#int i: 44($sp)
+	#int j[10]: 4($sp)
 	#int .v0: 0($sp)
+	LI $5, 0	#Number recognised:0
+	LW $6, 44($sp)	#Local Variable recognised:i
+	SW $5, 44($sp)	#Assign one register to another
+	LW $5, 4($sp)	#Local Variable recognised:j
+	LW $6, 44($sp)	#Local Variable recognised:i
+	SW $5, 44($sp)	#Assign one register to another
+#Error: Unrecognised command 21.
+#Error: Unrecognised command 21.
 #Error: Unrecognised command 22.
 	LI $5, 1	#Number recognised:1
 	LW $6, 0($sp)	#Local Variable recognised:.v0
-	ADD $14, $5, $6	#Add 2 Variables and store result int temp register
-	LW $15, 4($sp)	#Local Variable recognised:i
-	SW $14, 4($sp)	#Assign one register to another
+	ADD $15, $5, $6	#Add 2 Variables and store result int temp register
+	LW $7, 44($sp)	#Local Variable recognised:i
+	SW $15, 44($sp)	#Assign one register to another
 	#End of function main. We will restore the return adress $31 and the $fp. Then we will jump back to where the func was called.
 	LW $fp, 0($sp)
 	LW $31, 4($sp)
--- test-files/test02.c
+++ test-files/test02.c
@@ -1,10 +1,13 @@
-int f() 
+int f(int i, int j[10]) 
 {
-	return 1;
+	return i;
 } 
 
 int main() 
 {
 	int i; 
-	i = 1 + f();
+	int j[10];
+	i = 0;
+	i = j;
+	i = 1 + f(i, j); //Arrays will be passed by reference (as a pointer) not by value (as a copy)
 }
